3e18d0b9e63bb96f0986ceba34047d241f701c6f
krajansoft <krajansoft@gmail.com>
2017-05-23 22:34:44 +0200

    add

--- sendbox/src/main/java/pl/krajan/sendbox/MyFirstProgram.java
+++ sendbox/src/main/java/pl/krajan/sendbox/MyFirstProgram.java
@@ -1,10 +1,9 @@
 package pl.krajan.sendbox;
 
-public class MyFirstProgram
-{
-	
-	public static void main(String[] args) {
-		
-		System.out.println("Hello, world");
-	}
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world");
+    }
 }
