cf792ee3a4ffca6f237648172fa63f6326f7f24a
krajansoft <krajansoft@gmail.com>
2017-05-23 22:45:19 +0200

    add mathematic

--- sendbox/src/main/java/pl/krajan/sendbox/MyFirstProgram.java
+++ sendbox/src/main/java/pl/krajan/sendbox/MyFirstProgram.java
@@ -5,5 +5,10 @@ public class MyFirstProgram {
     public static void main(String[] args) {
 
         System.out.println("Hello, world");
+
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2-2);
+        System.out.println(2/2);
     }
 }
