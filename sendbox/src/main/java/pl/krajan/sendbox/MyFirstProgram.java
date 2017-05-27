da6964d258cb0e8df6f8c24b4b13c2801013e894
krajansoft <krajansoft@gmail.com>
2017-05-27 13:34:45 +0200

    add math funtion

--- sendbox/src/main/java/pl/krajan/sendbox/MyFirstProgram.java
+++ sendbox/src/main/java/pl/krajan/sendbox/MyFirstProgram.java
@@ -4,15 +4,35 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        String somebody = "Adam";
-        System.out.println("Hello " + somebody);
+      hello("Adam");
+      hello("User");
+      hello("Testy");
 
-        System.out.println("2 + 2 =  " + (2 + 2));
+      double len = 2;
+      double second = 3;
+      double third = 7;
+        System.out.println("Pole kwadratu o dlugosci " + len + " = " + wzorkwadratu(len));
+        System.out.println("Pole ptostokatu o dlugosci " + len + " i szerokosci " + second +" = " + prostokatu(len, second));
+        System.out.println("Pierwiastek z liczby " + len + " wynosi " + pierwiastek(len));
+        System.out.println("Liczba " + len + " podniesione do potegi " + third + " jest rowna " + potega(len, third));
+    }
 
-        double a = 4333;
-        double r = 300;
-        double t = (a*a)*r;
-
-        System.out.println("Witaj " + "Twój wynik jest równy " + t + " zł");
+    private static void hello(String somebody) {
+        System.out.println("Witaj " + somebody + "!");
+    }
+    public static double wzorkwadratu(double l){
+        return l*l;
+    }
+    public static double prostokatu(double a, double b){
+        return a*b;
+    }
+    public static double pierwiastek(double a){
+        return Math.sqrt(a);
+    }
+    public static double potega(double a, double b){
+        return Math.pow(a,b);
+    }
+    public static double trojkata(double a, double b){
+        return a*b;
     }
 }
