151977d8e0e01acce7c26b1906e9151198128051
krajansoft <krajansoft@gmail.com>
2017-05-28 15:22:29 +0200

    add class homework2

---
+++ sendbox/src/main/java/pl/krajan/sendbox/Homework2.java
@@ -0,0 +1,29 @@
+package pl.krajan.sendbox;
+
+public class Homework2 {
+
+    public static void main(String[] args) {
+
+      hello("User");
+
+      Squeare s = new Squeare(9);
+
+      Rectangle r = new Rectangle(10,2);
+
+      Triangle t = new Triangle(10);
+
+      Homework h = new Homework(120, 90, 7);
+
+        System.out.println("Pole kwadratu o dlugosci " + s.l + " = " + s.wzorkwadratu());
+        System.out.println("Pole ptostokatu o dlugosci " + r.a + " i szerokosci " + r.b +" = " + r.prostokatu());
+        System.out.println("HOMEWORK Sum " + h.u + " and " + h.w + " divided " + h.z + " results " + h.resoulthomerork());
+        System.out.println("Pierwiastek z liczby " + s.l + " wynosi " + t.pierwiastek(s.l));
+        System.out.println("Liczba " + r.a + " podniesione do potegi " + r.b + " jest rowna " + t.potega(r.a, r.b));
+        System.out.println("Wysokosc trojkata o podstawie " + t.alpha + " jest rowna " + t.wysokosctrojkata(t));
+        System.out.println("Pole trojkata rownobocznego, o boku " + t.alpha + " jest rowne " + t.poletrojkata(t));
+    }
+
+    private static void hello(String somebody) {
+        System.out.println("Witaj " + somebody + "!");
+    }
+}
