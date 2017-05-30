eea521b67075960b0d9d39d91203c97fa32d1fe0
krajansoft <krajansoft@gmail.com>
2017-05-30 21:21:20 +0200

    add Point class

--- sendbox/src/main/java/pl/krajan/sendbox/MyFirstProgram.java
+++ sendbox/src/main/java/pl/krajan/sendbox/MyFirstProgram.java
@@ -10,6 +10,9 @@ public class MyFirstProgram {
 
       Rectangle r = new Rectangle(10,2);
 
+      Point p = new Point(new double[]{3,2}, new double[]{2,2});
+
+        System.out.println("Punkty " + p.p[0] + " " +  p.p[1] + " i " + p.k[0] +" " + p.k[1] + " = " + p.pole());
         System.out.println("Pole kwadratu o dlugosci " + s.l + " = " + s.wzorkwadratu());
         System.out.println("Pole ptostokatu o dlugosci " + r.a + " i szerokosci " + r.b +" = " + r.prostokatu());
     }
