7956637dfd7ac110d7e0d8e2148fab9061b8b4bc
krajansoft <krajansoft@gmail.com>
2017-06-08 22:26:32 +0200

    add 2.6

--- addressbook/src/test/java/pl/krajan/GroupCreationTest.java
+++ addressbook/src/test/java/pl/krajan/GroupCreationTest.java
@@ -21,10 +21,6 @@ public class GroupCreationTest {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTest() {
         wd.get("http://localhost:8080/addressbook/");
         wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.name("user")).click();
@@ -34,6 +30,11 @@ public class GroupCreationTest {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testGroupCreation() {
+
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
