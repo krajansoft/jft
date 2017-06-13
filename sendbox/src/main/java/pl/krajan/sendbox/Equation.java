8553c66834d4919be7b2a4d6431d451b5140cd27
krajansoft <krajansoft@gmail.com>
2017-06-13 22:53:47 +0200

    add 3.2

--- sendbox/src/main/java/pl/krajan/sendbox/Equation.java
+++ sendbox/src/main/java/pl/krajan/sendbox/Equation.java
@@ -1,5 +1,7 @@
 package pl.krajan.sendbox;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * Created by kraja on 2017-06-13.
  */
@@ -10,27 +12,42 @@ public class Equation {
 
     private int n;
 
-    public Equation(double a, double b, double c){
+    public Equation(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
 
-        double d = b*b - 4*a*c;
+        double d = b * b - 4 * a * c;
 
-        if(d > 0) {
-
-            n = 2;
-        } else {
-            if (d ==0){
-                n = 1;
-            }
-            else {
-                if (d < 0){
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    n = -1;
+                } else {
                     n = 0;
                 }
             }
+
+            else {
+                    n = 1;
+                }
+            } else
+
+            {
+                if (d > 0) {
+                    n = 2;
+                } else if (d == 0) {
+                    n = 1;
+                } else {
+                    n = 0;
+                }
+
+
+            }
+
         }
-    }
+
+
     public int rootNumber() {
         return n;
     }
