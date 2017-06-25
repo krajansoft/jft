500c14bde56633db6ad06d703646db00b154febb
krajansoft <krajansoft@gmail.com>
2017-06-25 16:25:36 +0200

    add after 4.8 lession
    creation group

--- addressbook/src/test/java/pl/krajan/tests/GroupCreationTest.java
+++ addressbook/src/test/java/pl/krajan/tests/GroupCreationTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.krajan.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -14,9 +15,20 @@ public class GroupCreationTest extends TestBase {
     public void testGroupCreation() {
         app.getNawigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("AdamQA1", "Q", null));
+        GroupData group = new GroupData("AdamQA1", "Q", null);
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        int max = 0;
+        for (GroupData g : after) {
+            if (g.getId() > max){
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 }
