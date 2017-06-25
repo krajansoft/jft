1d4136d7c0e9193b541450d63ec8901823d64d00
krajansoft <krajansoft@gmail.com>
2017-06-25 20:08:12 +0200

    homework after 4.6 lession
    generowanie to string, assert equals.

--- addressbook/src/test/java/pl/krajan/model/ContactData.java
+++ addressbook/src/test/java/pl/krajan/model/ContactData.java
@@ -52,4 +52,30 @@ public class ContactData {
     public String getGroup() {
         return group;
     }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstname != null ? firstname.hashCode() : 0;
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        return result;
+    }
 }
--- addressbook/src/test/java/pl/krajan/tests/ContactDeletionTest.java
+++ addressbook/src/test/java/pl/krajan/tests/ContactDeletionTest.java
@@ -23,6 +23,9 @@ public class ContactDeletionTest extends TestBase {
     app.getNawigationHelper().goToHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
     }
 
 }
