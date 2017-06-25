eafbbdd84c593f29ea9bbd557c845714dd967734
krajansoft <krajansoft@gmail.com>
2017-06-25 17:57:03 +0200

    add code from lession 4.10

--- addressbook/src/test/java/pl/krajan/model/GroupData.java
+++ addressbook/src/test/java/pl/krajan/model/GroupData.java
@@ -6,8 +6,18 @@ public class GroupData {
     private final String header;
     private final String footer;
 
+
+
     public GroupData (String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public GroupData(int id, String name, String header, String footer) {
+
+        this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -18,13 +28,6 @@ public class GroupData {
         this.id = id;
     }
 
-    public GroupData(int id, String name, String header, String footer) {
-
-        this.id = id;
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
-    }
 
     public int getId() {
         return id;
@@ -44,29 +47,26 @@ public class GroupData {
     }
 
     @Override
-    public String toString() {
-        return "GroupData{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         GroupData groupData = (GroupData) o;
 
-        if (id != groupData.id) return false;
         return name != null ? name.equals(groupData.name) : groupData.name == null;
     }
 
     @Override
     public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
     }
 
 }
--- addressbook/src/test/java/pl/krajan/tests/GroupCreationTest.java
+++ addressbook/src/test/java/pl/krajan/tests/GroupCreationTest.java
@@ -4,6 +4,8 @@ package pl.krajan.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.krajan.model.GroupData;
+
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,7 +23,10 @@ public class GroupCreationTest extends TestBase {
 
         group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 
 }
