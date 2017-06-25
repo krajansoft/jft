21037ed643c1f85847c133a9b455a3b243ffd046
krajansoft <krajansoft@gmail.com>
2017-06-25 15:41:17 +0200

    add code after lession 4.7
    modyfikacja grupy

--- addressbook/src/test/java/pl/krajan/tests/GroupModificationTest.java
+++ addressbook/src/test/java/pl/krajan/tests/GroupModificationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.krajan.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -20,11 +21,18 @@ public class GroupModificationTest extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() -1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("AdamQA1", "Header test", "Footer test"));
+        GroupData group = new GroupData(before.get(before.size() -1).getId(),"AdamQA1", "Header test", "Footer test");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() -1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+
     }
 
 }
