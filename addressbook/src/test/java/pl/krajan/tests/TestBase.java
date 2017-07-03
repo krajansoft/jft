ffe46490511fa1266cbf4f9546735e5ba6b50ee9
krajansoft <krajansoft@gmail.com>
2017-07-03 22:03:36 +0200

    code after 5.1 lession

--- addressbook/src/test/java/pl/krajan/tests/TestBase.java
+++ addressbook/src/test/java/pl/krajan/tests/TestBase.java
@@ -2,7 +2,9 @@ package pl.krajan.tests;
 
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import pl.krajan.appmanager.ApplicationManager;
 
 /**
@@ -10,14 +12,14 @@ import pl.krajan.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown() {
         app.stop();
     }
